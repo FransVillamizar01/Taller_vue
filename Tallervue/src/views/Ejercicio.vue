2a6005a6a71dfde6fbb1cffd58b95ba4672e0189
Frans Sebastian Villamizar Maldonado <fsvillamizar@uniboyaca.edu.co>
2023-09-15 17:30:53 -0500

    Con diseño

--- Tallervue/src/views/Ejercicio.vue
+++ Tallervue/src/views/Ejercicio.vue
@@ -43,7 +43,7 @@
           </table>
           <div class="total">
             <p>Total de la Compra: ${{ calculateTotal() }}</p>
-            <p>Descuento: ${{ calculateDiscount() }}</p>
+            <p>Descuento: {{ calculateDiscountPercentage() }}%</p> <!-- Mostrar el porcentaje de descuento -->
             <p>Total a Pagar: ${{ calculateTotalWithDiscount() }}</p>
           </div>
         </div>
@@ -85,30 +85,30 @@
         }
         return total.toFixed(2);
       },
-      calculateDiscount() {
+      calculateDiscountPercentage() {
         let totalQuantity = 0;
         let totalAmount = 0;
         for (const product of this.products) {
           totalQuantity += product.quantity;
           totalAmount += product.quantity * product.price;
         }
-        let discount = 0;
+        let discountPercentage = 0;
         if (totalQuantity >= 12) {
-          discount = 0.2; // 20% discount for 12 or more items
+          discountPercentage = 20; // 20% discount for 12 or more items
         } else if (totalQuantity >= 6) {
-          discount = 0.1; // 10% discount for 6 or more items
+          discountPercentage = 10; // 10% discount for 6 or more items
         } else if (totalAmount >= 240000) {
-          discount = 0.15; // 15% discount for total amount >= 240,000
+          discountPercentage = 15; // 15% discount for total amount >= 240,000
         } else if (totalAmount >= 120000) {
-          discount = 0.1; // 10% discount for total amount >= 120,000
+          discountPercentage = 10; // 10% discount for total amount >= 120,000
         } else if (totalAmount >= 60000) {
-          discount = 0.05; // 5% discount for total amount >= 60,000
+          discountPercentage = 5; // 5% discount for total amount >= 60,000
         }
-        return (totalAmount * discount).toFixed(2);
+        return discountPercentage;
       },
       calculateTotalWithDiscount() {
         const totalAmount = parseFloat(this.calculateTotal());
-        const discount = parseFloat(this.calculateDiscount());
+        const discount = (parseFloat(this.calculateDiscountPercentage()) / 100) * totalAmount;
         return (totalAmount - discount).toFixed(2);
       },
     },
@@ -131,21 +131,44 @@
   }
   
   .input-section {
-    background-color: #f0f0f0;
+    background-color: #0000005b;
     padding: 20px;
     border-radius: 5px;
     margin-bottom: 20px;
   }
   
-  /* ... Otros estilos ... */
+  .input-box {
+    margin-bottom: 10px;
+  }
+  
+  button {
+    background-color: #363636;
+    color: #fff;
+    border: none;
+    padding: 10px 20px;
+    cursor: pointer;
+  }
+  
+  button:hover {
+    background-color: #b30000;
+  }
   
   .product-list table {
     width: 100%;
     border-collapse: collapse;
-    margin: 0 auto; /* Centra la tabla */
-    text-align: left; /* Justifica el texto a la izquierda en la tabla */
   }
   
-  /* ... Otros estilos ... */
+  .product-list th, .product-list td {
+    padding: 8px 12px;
+    border-bottom: 1px solid #ddd;
+  }
+  
+  .product-list th {
+    background-color: #000000;
+  }
+  
+  .total {
+    margin-top: 20px;
+  }
   </style>
   
